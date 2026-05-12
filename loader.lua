8a9598cf44507624e1b57c2abda071adc5e524b1
asp1_rin <acorn0757@gmail.com>
2026-05-12 17:23:53 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/asp1-rin/rivals-pixel-hub/main/src/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/asp1-rin/rivals-pixel-hub/refs/heads/main/src/main.lua"))()
